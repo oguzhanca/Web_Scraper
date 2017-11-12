b70b387cdba7b8a6313abe3b613e0fa8d288fea2
Oguzhan Calikkasap <oguzhanca94@gmail.com>
2017-11-12 22:48:54 +0300

    Extract Address and City information

--- backend.py
+++ backend.py
@@ -26,12 +26,15 @@ def requestPage(base_URL):
 
     # """Find number of results found"""
     totalResult_num = int(soup.find("div",{"class":"results-label"}).get("data-count"))
-    
+
     l=[]
     S_max = totalResult_num // 20
-    print(S_max)
-    for i in range(0, 40, 20):#S_max*20, 20):
-        newUrl = base_URL + "?s=" + str(i) + "&o=listingdate-desc"
+    print("remainder is: ", totalResult_num % 20)
+    print("Number of total results: ", totalResult_num)
+    print("S_max is: ", S_max)
+
+    for page in range(0,40,20):# S_max*20, 20):
+        newUrl = base_URL + "?s=" + str(page) + "&o=listingdate-desc"
         r = requests.get(newUrl)
         c = r.content
         soup = BeautifulSoup(c,"html.parser")
@@ -40,10 +43,21 @@ def requestPage(base_URL):
         for item in all:
             d= {}
             d["Price"]=item.find("a",{"class":"listing-price"}).text.strip()
+            d["Address"]=item.find("div",{"class":"property-address"}).text.strip()
+            try:
+                d["City"]=item.find("div",{"class":"property-city"}).text.strip()
+            except:
+                d["City"]=None
+            # try:
+            #     d["City"]=item.find("div",{"class":"property-address"}).text.strip()
+            # except:
+            #     d["City"]=None
             l.append(d)
 
+
+
     print(l)
-    print(len(l))
+    print("Length of array: ", len(l))
 
     # print("last item's price: ", all[-1].find("a",{"class":"listing-price"}).text.strip())
 
